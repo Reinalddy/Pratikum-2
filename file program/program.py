b9e687e8092b1f256c404d72774f34ce93eb2734
Reinalddy <Reinalddyaldy183@gmail.com>
2021-10-31 12:19:10 +0700

    menambahkan file program

---
+++ file program/program.py
@@ -0,0 +1,21 @@
+#biodata mahasiswa
+print ("program menghitung luas lingkaran dan keliling lingkarang \n")
+print ("nama Reinalddy\n NIm:312110522")
+
+#luas lingkaran
+phi = 3.14
+r = float (input ("masukan panjang jari jari lingkaran : "))
+luas_lingkaran = phi*r*r
+
+print ("luas lingkaran adalah :",luas_lingkaran)
+
+#keliling lingkaran
+r = float (input ( "masukan jari jari lingkaran :"))
+keliling_lingkaran = phi*r
+
+print ("keliling lingkaran adalah :" , keliling_lingkaran)
+
+
+
+
+
